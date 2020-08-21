fec555b2bb174f8bfb21caf534fd1367b5399a3e
creationyun <creationyun@gmail.com>
2020-08-22 01:27:26 +0900

    Delete unzip_to_dataset

--- unzip_to_dataset/unzip_to_dataset.h
+++
@@ -1,8 +0,0 @@
-#include <cstdio>
-#include <zlib.h>
-#include <dirent.h>
-#include <cstdlib>
-#include <cstring>
-#include <libgen.h>
-
-int inflate_mv_dataset(const char *gz_filepath, const char *maldataset_dir, const char *original_filename);
